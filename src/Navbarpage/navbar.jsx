11ba49dbd8658b95116ae351a3d1e5792f58d805
govind <govind@gmail.com>
2025-12-14 11:40:29 +0530

    update

--- src/Navbarpage/navbar.jsx
+++ src/Navbarpage/navbar.jsx
@@ -6,7 +6,7 @@ const NavbarPage=()=>{
     return(
         <div className="nav">
             <div className="navbar">
-                <h1 className="logo">NIROGGYAN</h1> 
+                <h1 className="logo">SAHASRA</h1> 
                 <img className="logoimage" src="https://i.pinimg.com/236x/28/91/af/2891af997f762f983aa522e0e98c36aa.jpg"/>
             </div>
         </div>
